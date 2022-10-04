42b38e0c3db5381cfc2295209f88d93493017c49
David Trpchevski <trpcevski.david@gmail.com>
2022-10-04 11:43:39 +0200

    feat: Add to top button
    Signed-off-by: David Trpchevski <trpcevski.david@gmail.com>

--- app/src/main/java/com/david/filmobil/home/HomeFragment.kt
+++ app/src/main/java/com/david/filmobil/home/HomeFragment.kt
@@ -6,6 +6,7 @@ import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.crazylegend.viewbinding.viewBinding
 import com.david.filmobil.R
@@ -67,5 +68,18 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
                 )
             }
         }
+
+        binding.toTop.setOnClickListener {
+            binding.moviesList.layoutManager?.scrollToPosition(0)
+        }
+
+        binding.moviesList.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                val position =
+                    (binding.moviesList.layoutManager as LinearLayoutManager).findFirstVisibleItemPosition()
+                binding.toTop.isVisible = position >= 20
+            }
+        })
     }
 }
